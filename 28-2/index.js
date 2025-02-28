7b549c42dffd73668d6869e708eeb78a3eac35b8
SandhyaYenkikar <167804864+SandhyaYenkikar@users.noreply.github.com>
2025-02-28 14:59:58 +0530

    Add files via upload

---
+++ 28-2/index.js
@@ -0,0 +1,40 @@
+//1st code (fs module)
+/*create file
+const fs  = require('fs');
+fs.writeFileSync('C:\Users\SandhyaYenkikar(QCIN\Desktop\my.txt',"Hello my first file");
+console.log('file created');*/
+
+
+//2nd. module create
+// function add (a,b)
+// {
+//     return a+b;
+// }
+// function multiply (a,b){
+//     return a*b;
+// }
+// module.exports={add,multiply};
+
+
+//3rd. Creating Package
+// function logMessage(msg){
+//     console.log(`[LOG]:${msg}`)
+// }
+
+//using express.js for simple server
+// const express = require('express');
+// const app = express ();
+
+// app.get('/',(req, res) => {
+//     res.end('hello, world');
+// });
+
+// app.get('/node',(req, res) => {
+//     res.end('node learning');
+// });
+// app.get('/sandhya',(req, res) => {
+//     res.end('hello sandhya');
+// });
+
+// app.listen(3000, () =>console.log("Server Running on port 3000"));
+
---
+++ 28-2/index2.js
@@ -0,0 +1,10 @@
+
+//const index=require('./index')
+
+//creating module
+//  console.log(index.add(10,2));
+//  console.log(index.multiply(10,3));
+
+
+//creating api
+//console.log(logMessage("Hello"));
